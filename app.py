a3480f45c66a1339d7e2405f9786c815267f4798
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-04-15 18:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,14 +44,14 @@ mdlit('## ⚙️ My [green]Work[/green] & [green]Projects[/green]')
 
 first = "I've worked on lots of projects based on Machine Learning Operations , Classification, Regression and NLP"
 with st.expander(first):
-    mdlit("- [red]**Mobile Price Classification**[/red] - It is a [blue]Machine Learning Classification[/blue] where I've preprocessed the data and build the model with much more accuracy. It is predicting the price range of Mobile Phone if user inputs the all specification of Mobile phone.")
-    mdlit("- [red]Sentiment Analysis[/red] - It is a [blue]Natural Language Processing[/blue] project in which model is going to predict the sentiment of the news that user typed. ")
-    mdlit("- [red]Pipeline Generator[/red] - As we know, Doing repititive process of preprocessing and hyperparameter tune the model is very tedious. So it is a utility which generates a [blue]python[/blue] code depicting the code for preprocessing pipeline.")
+    mdlit("- [yellow]**Mobile Price Classification**[/yellow] - It is a [blue]Machine Learning Classification[/blue] where I've preprocessed the data and build the model with much more accuracy. It is predicting the price range of Mobile Phone if user inputs the all specification of Mobile phone.")
+    mdlit("- [yellow]Sentiment Analysis[/yellow] - It is a [blue]Natural Language Processing[/blue] project in which model is going to predict the sentiment of the news that user typed. ")
+    mdlit("- [yellow]Pipeline Generator[/yellow] - As we know, Doing repititive process of preprocessing and hyperparameter tune the model is very tedious. So it is a utility which generates a [blue]python[/blue] code depicting the code for preprocessing pipeline.")
     mdlit("📎 View the whole app consisting this [projects](https://hg03-mlperweek-main-r82uqu.streamlit.app/)")
 second = "I've worked on lots of projects based on Machine Learning Operations"
 with st.expander(second):
-    mdlit("- [red]OpenAI exploration[/red] - I've created a project which consist of conversational chatbot using [blue]openai[/blue] and [blue]lanchain[/blue] as well as mimic of [blue]Dalle 2[/blue]")
-    mdlit("- [red]Life Expectany Prediction[/red] - I've created a [blue]Regression[/blue] model which is predicting the rate of life expectancy of person based on **alcohol consumption**, **adult mortality**, **country** etc.")
+    mdlit("- [yellow]OpenAI exploration[/yellow] - I've created a project which consist of conversational chatbot using [blue]openai[/blue] and [blue]lanchain[/blue] as well as mimic of [blue]Dalle 2[/blue]")
+    mdlit("- [yellow]Life Expectany Prediction[/yellow] - I've created a [blue]Regression[/blue] model which is predicting the rate of life expectancy of person based on **alcohol consumption**, **adult mortality**, **country** etc.")
     mdlit("📎 View the whole app consisting this [projects](https://mlexhaust.streamlit.app/)")
 third = "I've worked on lots of projects based on Machine Learning Operations"
 with st.expander(third):
